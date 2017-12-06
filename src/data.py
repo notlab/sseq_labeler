edf76c065ae8cf63853fab0f3d7ebc7426268d41
Gideon Providence <g@math.utoronto.ca>
2017-12-06 11:24:42 -0500

    generator nn

--- src/data.py
+++ src/data.py
@@ -1,8 +1,12 @@
 import os
+import random
 import unicodedata
 import string
 import glob
 from io import open
+
+import torch
+from torch import autograd
 
 BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 DATA_REGX = '../data/lstm/names/*.txt'
@@ -22,20 +26,49 @@ def _read_lines(filename):
 def _getidx(char):
     return CHARS.find(char)
 
-def line_to_tensor(line):
-    tensor = torch.zeros(len(line), 1, N_CHARS)
-    for li, char in enumerate(line):
-        tensor[li][0][getidx(char)] = 1
+def decode_prediction(categories, pred):
+    _, top_i = pred.data.topk(1) 
+    category_i = top_i[0][0]
+    return categories[category_i], category_i
+
+def randelem(lst):
+    return lst[random.randint(0, len(lst) - 1)]
+
+def random_sample(categories, examples, one_hot_categories=False):
+    category = randelem(categories)
+    category_tensor = None
+    sample = randelem(examples[category])
+    sample_tensor = autograd.Variable(sample_to_tensor(sample))
+    
+    if one_hot_categories:
+        idx = categories.index(category)
+        category_tensor = torch.zeros(1, len(categories))
+        category_tensor[0][idx] = 1
+        category_tensor = autograd.Variable(category_tensor)
+    else:
+        category_tensor = autograd.Variable(torch.LongTensor([categories.index(category)]))
+        
+    return category, sample, category_tensor, sample_tensor
+
+def sample_to_tensor(sample):
+    tensor = torch.zeros(len(sample), 1, N_CHARS)
+    for si, char in enumerate(sample):
+        tensor[si][0][_getidx(char)] = 1
     return tensor
 
+def target_encode(sample):
+    indicies = [CHARS.find(line[idx]) for idx in range(1, len(idx))]
+    indicies.append(N_CHARS - 1)
+    return torch.LongTensor(indicies)
+
 def inputs():
-    category_lines = {}
-    all_categories = []
+    categories = []
+    examples = {}
 
     for filename in glob.glob(DATA_PATH):
         category = filename.split('/')[-1].split('.')[0]
-        all_categories.append(category)
+        categories.append(category)
         lines = _read_lines(filename)
-        category_lines[category] = lines
+        examples[category] = lines
 
-    return category_lines, all_categories
+    return categories, examples
--- src/model.py
+++ src/model.py
@@ -1,52 +1,126 @@
-import os
-import unicodedata
-import string
-import glob
-from io import open
-
 import torch
+from torch import nn, autograd
 
-BASE_PATH = os.path.dirname(os.path.abspath(__file__))
-DATA_REGX = '../data/lstm/names/*.txt'
-DATA_PATH = os.path.join(BASE_PATH, DATA_REGX)
+import data
 
-CHARS = string.ascii_letters + " .,;'"
-N_CHARS = len(CHARS)
+class LabellerNN(nn.Module):
+    
+    def __init__(self, input_size, hidden_size, output_size, criterion=nn.NLLLoss(), lr=0.005):
+        super(LabellerNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
+        self.i2o = nn.Linear(input_size + hidden_size, output_size)
+        self.softmax = nn.LogSoftmax()
+        self.criterion = criterion
+        self.lr = lr
 
-def readLines(filename):
-    lines = open(filename, encoding='utf-8').read().strip().split('\n')
-    return [unicodeToAscii(line) for line in lines]
+    def forward(self, ins, hidden):
+        combined = torch.cat((ins, hidden), 1)
+        hidden = self.i2h(combined)
+        output = self.i2o(combined)
+        output = self.softmax(output)
+        return output, hidden
 
-def unicodeToAscii(s):
-    return ''.join(c for c in unicodedata.normalize('NFD', s)
-                   if unicodedata.category(c) != 'Mn' and c in CHARS)
+    def init_hidden(self):
+        return autograd.Variable(torch.zeros(1, self.hidden_size))
+    
+    def train(self, category_tensor, sample_tensor):
+        hidden = self.init_hidden()
 
-def getidx(char):
-    return CHARS.find(char)
+        self.zero_grad()
 
-def asTensor(char):
-    tensor = torch.zeros(1, N_CHARS)
-    tensor[0][getidx(char)] = 1
-    return tensor
+        for i in range(sample_tensor.size()[0]):
+            output, hidden = self.__call__(sample_tensor[i], hidden)
 
-def lineToTensor(line):
-    tensor = torch.zeros(len(line), 1, N_CHARS)
-    for li, char in enumerate(line):
-        tensor[li][0][getidx(char)] = 1
-    return tensor
+        loss = self.criterion(output, category_tensor)
+        loss.backward()
 
-def inputs():
-    category_lines = {}
-    all_categories = []
+        # Add parameters' gradients to their values, multiplied by learning rate
+        for p in self.parameters():
+            p.data.add_(-self.lr, p.grad.data)
+                
+        return output, loss.data[0]
 
-    for filename in glob.glob(DATA_PATH):
-        category = filename.split('/')[-1].split('.')[0]
-        all_categories.append(category)
-        lines = readLines(filename)
-        category_lines[category] = lines
+class GeneratorNN(nn.Module):
+    
+    def __init__(self, input_size, hidden_size, output_size):
+        super(RNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.i2h = nn.Linear(n_categories + input_size + hidden_size, hidden_size)
+        self.i2o = nn.Linear(n_categories + input_size + hidden_size, output_size)
+        self.o2o = nn.Linear(hidden_size + output_size, output_size)
+        self.dropout = nn.Dropout(0.1)
+        self.softmax = nn.LogSoftmax()
 
-    return category_lines, all_categories
+    def forward(self, category, input, hidden):
+        input_combined = torch.cat((category, input, hidden), 1)
+        hidden = self.i2h(input_combined)
+        output = self.i2o(input_combined)
+        output_combined = torch.cat((hidden, output), 1)
+        output = self.o2o(output_combined)
+        output = self.dropout(output)
+        output = self.softmax(output)
+        return output, hidden
 
-category_lines, all_categories = inputs()
+    def init_hidden(self):
+        return autograd.Variable(torch.zeros(1, self.hidden_size))
 
-print(lineToTensor('Jones').size())
+    def train(category_tensor, input_line_tensor, target_line_tensor):
+        hidden = self.init_hidden()
+
+        self.zero_grad()
+
+        loss = 0
+
+        for i in range(input_line_tensor.size()[0]):
+            output, hidden = self.__call__(category_tensor, input_line_tensor[i], hidden)
+            loss += criterion(output, target_line_tensor[i])
+
+        loss.backward()
+
+        for p in self.parameters():
+            p.data.add_(-learning_rate, p.grad.data)
+
+        return output, loss.data[0] / input_line_tensor.size()[0]
+
+    
+import time
+import math
+
+n_hidden = 128
+n_iters = 100000
+print_every = 500
+plot_every = 100
+
+# Keep track of losses for plotting
+current_loss = 0
+all_losses = []
+
+def time_since(since):
+    now = time.time()
+    s = now - since
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
+start = time.time()
+
+categories, examples = data.inputs()
+n_categories = len(categories)
+rnn = LabellerNN(data.N_CHARS, n_hidden, n_categories, lr=0.01)
+
+for i in range(1, n_iters + 1):
+    category, sample, category_tensor, sample_tensor = data.random_sample(categories, examples)
+    pred, loss = rnn.train(category_tensor, sample_tensor)
+    current_loss += loss
+
+    # Print iter number, loss, name and guess
+    if i % print_every == 0:
+        guess, guess_i = data.decode_prediction(categories, pred)
+        correct = '✓' if guess == category else '✗ (%s)' % category
+        print('%d %d%% (%s) %.4f %s / %s %s' % (i, i / n_iters * 100, time_since(start), loss, sample, guess, correct))
+
+    # Add current loss avg to list of losses
+    if i % plot_every == 0:
+        all_losses.append(current_loss / plot_every)
+        current_loss = 0
