4d482f479a4d6534b696147438584fbf0b0438b8
Gideon Providence <g@math.utoronto.ca>
2017-12-17 14:39:32 -0500

    lstm train and results

--- src/model.py
+++ src/model.py
@@ -1,8 +1,10 @@
 import time
 import math
 
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 import torch
-from torch import nn, autograd
+from torch import nn, autograd, optim
 
 import data
 
@@ -41,7 +43,7 @@ class LabellerNN(nn.Module):
         # Add parameters' gradients to their values, multiplied by learning rate
         for p in self.parameters():
             p.data.add_(-self.lr, p.grad.data)
-                
+
         return output, loss.data[0]
 
 class LabellerLSTM(nn.Module):
@@ -65,8 +67,8 @@ class LabellerLSTM(nn.Module):
     def forward(self, ins, hidden):
         lstm_out, hidden = self.i2h_lstm(ins, hidden)
         direct_out = self.i2h(ins)
-        combined_out = torch.cat((lstm_out, direct_out), 1)
-        output = self.h2o(combined_out)
+        combined_out = torch.cat((lstm_out, direct_out), 2)
+        output = self.h2o(combined_out).view(1, self.out_dim)
         output = self.softmax(output)
         return output, hidden
 
@@ -83,6 +85,7 @@ class LabellerLSTM(nn.Module):
         for i in range(sample_tensor.size()[0]):
             output, hidden = self.__call__(sample_tensor[i].view(1, 1, self.in_dim), hidden)
 
+        output = output.view((1, self.out_dim))
         loss = self.criterion(output, category_tensor)
         loss.backward()
         optimizer.step()
@@ -142,6 +145,17 @@ def time_since(since):
     s -= m * 60
     return '%dm %ds' % (m, s)
 
+def evaluate(model, sample_tensor, reshape_for_minibatch=False):
+    hidden = model.init_hidden()
+    
+    for i in range(sample_tensor.size()[0]):
+        if reshape_for_minibatch:
+            pred, hidden = model(sample_tensor[i].view(1, 1, model.in_dim), hidden)
+        else:
+            pred, hidden = model(sample_tensor[i], hidden)
+
+    return pred
+
 def train_labeller():
     n_hidden = 128
     n_iters = 100000
@@ -173,6 +187,84 @@ def train_labeller():
             all_losses.append(current_loss / plot_every)
             current_loss = 0
 
+    plot_losses(all_losses, save=True, fname='simple_rnn_losses.png')
+    plot_confusion(rnn, categories, examples, fname='simple_rnn_confusion.png', reshape_for_minibatch=False)
+
+def train_lstm():
+    n_hidden = 128
+    n_iters = 100000
+    print_every = 500
+    plot_every = 100
+
+    current_loss = 0
+    all_losses = []
+
+    categories, examples = data.inputs()
+    n_categories = len(categories)
+    lstm = LabellerLSTM(data.N_CHARS, 64, 64, n_categories, lr=0.1)
+
+    start = time.time()
+
+    for i in range(1, n_iters + 1):
+        category, sample, category_tensor, sample_tensor = data.random_sample(categories, examples)
+        pred, loss = lstm.train(category_tensor, sample_tensor)
+        current_loss += loss
+
+        # Print iter number, loss, name and guess
+        if i % print_every == 0:
+            guess, guess_i = data.decode_prediction(categories, pred)
+            correct = '✓' if guess == category else '✗ (%s)' % category
+            print('%d %d%% (%s) %.4f %s / %s %s' % (i, i / n_iters * 100, time_since(start), loss, sample, guess, correct))
+
+        # Add current loss avg to list of losses
+        if i % plot_every == 0:
+            all_losses.append(current_loss / plot_every)
+            current_loss = 0
+
+    plot_losses(all_losses, save=True, fname='lstm_losses.png')
+    plot_confusion(lstm, categories, examples, fname='lstm_confusion.png', reshape_for_minibatch=True)
+    
+
+def plot_losses(all_losses, save=True, fname='losses'):
+    plt.figure(figsize=(20, 20))
+
+    if save:
+        plt.plot(all_losses)
+        plt.savefig(fname, bbox_inches='tight')
+    else:
+        plt.plot(all_losses)
+        plt.show()
+
+def plot_confusion(model, categories, examples, fname='confusion.png', reshape_for_minibatch=False):
+    n_categories = len(categories)
+    confusion = torch.zeros(n_categories, n_categories)
+    
+    for i in range(10000):
+        category, sample, category_tensor, sample_tensor = data.random_sample(categories, examples)
+        log_logits = evaluate(model, sample_tensor, reshape_for_minibatch=reshape_for_minibatch)
+        pred, pred_i = data.decode_prediction(categories, log_logits)
+        category_i = categories.index(category)
+        confusion[category_i][pred_i] += 1
+
+    # Normalize
+    for i in range(n_categories):
+        confusion[i] = confusion[i] / confusion[i].sum()
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    cax = ax.matshow(confusion.numpy())
+    fig.colorbar(cax)
+
+    ax.set_xticklabels([''] + categories, rotation=90)
+    ax.set_yticklabels([''] + categories)
+
+    # Force label at every tick
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+
+    plt.savefig(fname, bbox_inches='tight')
+
+
 def train_generator():
     n_iters = 100000
     print_every = 500
@@ -201,4 +293,5 @@ def train_generator():
             total_loss = 0
         
 train_labeller()
+#train_lstm()
 #train_generator()
